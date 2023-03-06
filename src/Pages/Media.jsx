2b357300072fe9e95a2e7381c5d76e13c130ae5d
chigoox <dikeemmanyel54@gmail.com>
2023-03-05 22:10:04 -0500

    a

--- src/Pages/Media.jsx
+++ src/Pages/Media.jsx
@@ -31,36 +31,36 @@ function Media() {
                     </div>
                 </div>
 
-                <div className='rounded-t-3xl m-auto bg-black h-fit w-fit relative flex flex-col items-center'>
-                    <h1 className='font-bold text-white text-center text-3xl m-2'>Videos</h1>
-                    <div className='flex'>
-                        <div className='h-content m-10 p-10'>
-                            <iframe className='w-[600px] border-4 h-[400px] relative bottom-14'
-                                src="https://www.youtube.com/embed/tgbNymZ7vqY">
-                            </iframe>
-                        </div>
-                        <div className='p-10 m-10'>
-                            <h1 className='font-bold text-white text-3xl'>Pastor Robert Jones, Jr.</h1>
-                            <h1 className='font-semibold text-slate-200 text-lg'>"Guard Your Heart"(Proverbs 4:23)</h1>
-
-                        </div>
-                    </div>
-                    <div className='flex'>
-                        <div className='p-10 m-10'>
-                            <h1 className='font-bold text-white text-3xl'>Leon deVose, Jr. John 1:11-14</h1>
-                        </div>
-                        <div className='h-content m-10 p-10'>
-                            <iframe className='w-[600px] border-4 h-[400px] relative bottom-14'
-                                src="https://www.youtube.com/embed/tgbNymZ7vqY">
-                            </iframe>
-                        </div>
-
-                    </div>
-                </div>
 
             </div>
 
 
+            <div className='rounded-t-3xl m-2 bg-black h-fit w-full relative flex flex-col items-center'>
+                <h1 className='font-bold text-white text-center text-3xl m-2'>Videos</h1>
+                <div className='flex lg:felx-row flex-col'>
+                    <div className='h-content m-10 p-10'>
+                        <iframe className='lg:w-[600px] border-4 lg:h-[400px] relative bottom-14'
+                            src="https://www.youtube.com/embed/tgbNymZ7vqY">
+                        </iframe>
+                    </div>
+                    <div className='p-10 m-10'>
+                        <h1 className='font-bold text-white text-3xl'>Pastor Robert Jones, Jr.</h1>
+                        <h1 className='font-semibold text-slate-200 text-lg'>"Guard Your Heart"(Proverbs 4:23)</h1>
+
+                    </div>
+                </div>
+                <div className='flex lg:felx-row flex-col' >
+                    <div className='p-10 m-10'>
+                        <h1 className='font-bold text-white text-3xl'>Leon deVose, Jr. John 1:11-14</h1>
+                    </div>
+                    <div className='h-content m-10 p-10'>
+                        <iframe className='lg:w-[600px] border-4 lg:h-[400px] relative bottom-14'
+                            src="https://www.youtube.com/embed/tgbNymZ7vqY">
+                        </iframe>
+                    </div>
+
+                </div>
+            </div>
 
         </div>
     )
